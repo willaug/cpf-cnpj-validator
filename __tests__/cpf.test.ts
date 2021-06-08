0986368b18615eeed77b42a4ed4805921179ec57
William Augusto <william.santos315@outlook.com>
2021-06-08 12:01:40 -0300

    validate CPF with tests (Jest)

--- __tests__/cpf.test.ts
+++ __tests__/cpf.test.ts
@@ -7,7 +7,12 @@ describe('CPF', () => {
   });
 
   it('Deve possuir 11 caracteres (Excluindo caracteres especiais)', () => {
-    const value = '123.456.789-13';
+    const value = '926.412.310-50';
+    expect(validator.isCPF(value)).toEqual(true);
+  });
+
+  it('Deve ser validado em uma das tentativas', () => {
+    const value = '502.597.100-40';
     expect(validator.isCPF(value)).toEqual(true);
   });
 });
--- src/validations/cpf.ts
+++ src/validations/cpf.ts
@@ -1,13 +1,33 @@
-const firstTry = (arr: number[]) => {
+const validateDigit = (arr: number[], position: number): boolean => {
+  let factor: number;
+  let arrayDigit: number;
   let sum = 0;
-  let factor = 10;
 
-  for (let i = 0; i < 9; i += 1) {
+  if (position === 1) {
+    factor = 10;
+    arrayDigit = 9;
+  } else {
+    factor = 11;
+    arrayDigit = 10;
+  }
+
+  for (let i = 0; i < arrayDigit; i += 1) {
     sum += arr[i] * factor;
     factor -= 1;
   }
 
-  return sum;
+  const division = Math.floor(sum % 11);
+  let verifyingDigit = 0;
+
+  if (division > 1) {
+    verifyingDigit = 11 - division;
+  }
+
+  if (arr[arrayDigit] !== verifyingDigit) {
+    return false;
+  }
+
+  return true;
 };
 
 const validateCPF = (cpf: unknown): boolean => {
@@ -25,8 +45,13 @@ const validateCPF = (cpf: unknown): boolean => {
   const arrCPF: number[] = Array.from(filteredCPF, Number);
 
   const repeatedNumbers: boolean = arrCPF.every((num, i, arr) => num === arr[0]);
-
   if (repeatedNumbers) {
+    return false;
+  }
+
+  const firstDigit = validateDigit(arrCPF, 1);
+  const secondDigit = validateDigit(arrCPF, 2);
+  if (!firstDigit || !secondDigit) {
     return false;
   }
 
